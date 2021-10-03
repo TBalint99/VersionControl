17b69afc6eb246c2212ad08761c60e3cb5a54828
Takács Bálint <tbalint99@gmail.com>
2021-10-03 17:15:30 +0200

    Added write function

--- UserMaintenance/UserMaintenance/Form1.cs
+++ UserMaintenance/UserMaintenance/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,6 +21,7 @@ namespace UserMaintenance
             lblLastName.Text = Resource_hu_en.LastName; // label1
             lblFirstName.Text = Resource_hu_en.FirstName; // label2
             btnAdd.Text = Resource_hu_en.Add; // button1
+            btnWrite.Text = Resource_hu_en.Write; //button2
             listUsers.DataSource = users;
             listUsers.ValueMember = "ID";
             listUsers.DisplayMember = "FullName";
@@ -33,6 +35,22 @@ namespace UserMaintenance
                 FirstName = txtFirstName.Text
             };
             users.Add(u);
+        }
+
+        private void btnWrite_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog sfd = new SaveFileDialog();
+            if (sfd.ShowDialog() != DialogResult.OK) return;
+            using (StreamWriter sw = new StreamWriter(sfd.FileName,false,Encoding.UTF8))
+            {
+                foreach (var s in users)
+                {
+                    sw.Write(s.ID);
+                    sw.Write(";");
+                    sw.Write(s.FirstName);
+                    sw.WriteLine();
+                }
+            }
         }
     }
 }
