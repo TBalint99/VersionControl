b06e5fb1c18f98fef5a094ec53dcdc0e340630fd
Takács Bálint <tbalint99@gmail.com>
2021-10-08 23:12:01 +0200

    Loading data

--- 4.gyak_wyaprm/4.gyak_wyaprm/Form1.cs
+++ 4.gyak_wyaprm/4.gyak_wyaprm/Form1.cs
@@ -12,9 +12,17 @@ namespace _4.gyak_wyaprm
 {
     public partial class Form1 : Form
     {
+        RealEstateEntities context = new RealEstateEntities();
+        List<Flat> flats;
         public Form1()
         {
             InitializeComponent();
+            LoadData();
+        }
+
+        public void LoadData()
+        {
+            flats = context.Flats.ToList();
         }
     }
 }
