4385cf692d605fade763944eee9e799106c3783f
Takács Bálint <tbalint99@gmail.com>
2021-10-08 23:37:59 +0200

    FormatTable, GetCelll and CreateTable added

--- 4.gyak_wyaprm/4.gyak_wyaprm/Form1.cs
+++ 4.gyak_wyaprm/4.gyak_wyaprm/Form1.cs
@@ -20,6 +20,17 @@ namespace _4.gyak_wyaprm
         Excel.Workbook xlWB;
         Excel.Worksheet xlSheet;
 
+        string[] headers = new string[] {
+                "Kód",
+                "Eladó",
+                "Oldal",
+                "Kerület",
+                "Lift",
+                "Szobák száma",
+                "Alapterület (m2)",
+                "Ár (mFt)",
+                "Négyzetméter ár (Ft/m2)"};
+
         public Form1()
         {
             InitializeComponent();
@@ -30,6 +41,8 @@ namespace _4.gyak_wyaprm
         {
             flats = context.Flats.ToList();
         }
+
+        object[,] values;
 
         public void CreateExcel()
         {
@@ -63,5 +76,88 @@ namespace _4.gyak_wyaprm
                 xlApp = null;
             }
         }
+        private void CreateTable()
+        {
+
+            for (int i = 0; i < headers.Length; i++)
+            {
+                xlSheet.Cells[1, i + 1] = headers[i];
+            }
+
+            values = new object[flats.Count, headers.Length];
+
+            int counter = 0;
+            foreach (Flat flat in flats)
+            {
+                values[counter, 0] = flat.Code;
+                values[counter, 1] = flat.Vendor;
+                values[counter, 2] = flat.Side;
+                values[counter, 3] = flat.District;
+                values[counter, 4] = flat.Elevator;
+                values[counter, 5] = flat.NumberOfRooms;
+                values[counter, 6] = flat.FloorArea;
+                values[counter, 7] = flat.Price;
+                //using GetCell for referencies
+                string value_1 = GetCell(counter + 2, 6);
+                string value_2 = GetCell(counter + 2, 7);
+                string value_merged = "=" + value_2 + "/" + value_1;
+                values[counter, 8] = value_merged;
+                counter++;
+            }
+
+            xlSheet.get_Range(
+            GetCell(2, 1),
+            GetCell(1 + values.GetLength(0), values.GetLength(1))).Value2 = values;
+
+            FormatTable();
+            FormatTable_own();
+        }
+        private string GetCell(int x, int y)
+        {
+            string ExcelCoordinate = "";
+            int dividend = y;
+            int modulo;
+
+            while (dividend > 0)
+            {
+                modulo = (dividend - 1) % 26;
+                ExcelCoordinate = Convert.ToChar(65 + modulo).ToString() + ExcelCoordinate;
+                dividend = (int)((dividend - modulo) / 26);
+            }
+            ExcelCoordinate += x.ToString();
+
+            return ExcelCoordinate;
+        }
+
+        private void FormatTable()
+        {
+            Excel.Range headerRange = xlSheet.get_Range(GetCell(1, 1), GetCell(1, headers.Length));
+            headerRange.Font.Bold = true;
+            headerRange.VerticalAlignment = Excel.XlVAlign.xlVAlignCenter;
+            headerRange.HorizontalAlignment = Excel.XlHAlign.xlHAlignCenter;
+            headerRange.EntireColumn.AutoFit();
+            headerRange.RowHeight = 40;
+            headerRange.Interior.Color = Color.LightBlue;
+            headerRange.BorderAround2(Excel.XlLineStyle.xlContinuous, Excel.XlBorderWeight.xlThick);
+        }
+
+        private void FormatTable_own()
+        {
+            //border
+            Excel.Range border = xlSheet.get_Range(GetCell(2, 1), GetCell(1 + values.GetLength(0), values.GetLength(1)));
+            border.BorderAround2(Excel.XlLineStyle.xlContinuous, Excel.XlBorderWeight.xlThick);
+
+            //first column
+            Excel.Range firstColumn = xlSheet.get_Range(GetCell(2, 1), GetCell(1 + values.GetLength(0), 1));
+            firstColumn.Font.Bold = true;
+            firstColumn.Interior.Color = Color.LightYellow;
+
+            //last column
+            Excel.Range lastColumn = xlSheet.get_Range(GetCell(2, 9), GetCell(1 + values.GetLength(0), 9));
+            lastColumn.Interior.Color = Color.LightGreen;
+
+        }
+
     }
+}
 }
