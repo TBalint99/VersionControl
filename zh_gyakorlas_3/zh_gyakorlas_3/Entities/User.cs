14bbb591f3124793937d24e3872ba77aab70ea9e
Takács Bálint <tbalint99@gmail.com>
2021-10-13 17:48:49 +0200

    Project created and first steps made.

---
+++ zh_gyakorlas_3/zh_gyakorlas_3/Entities/User.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//a .Entites csak akkor jelenik meg, ha a mappában van létrehozva a User.cs
+namespace zh_gyakorlas_3.Entities
+{
+    class User
+    {
+        //Kiírni --> Így lehet azonosítót generálni
+        public Guid ID { get; set; } = Guid.NewGuid();
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        //Kiírni --> Csak visszaadja a 2 nevet összefűzve
+        public string FullName
+        { 
+            get 
+            {
+                return string.Format(
+                    "{0} {1}",
+                    LastName,
+                    FirstName);
+            }
+        }
+
+    }
+}
---
+++ zh_gyakorlas_3/zh_gyakorlas_3/Form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace zh_gyakorlas_3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            //Kiírni, hogy hogyan kell Resource file-t hozzáadni
+            //Ezeket is kiírni:
+            label1.Text = Resource1.LastName;
+            label2.Text = Resource1.FirstName;
+            button1.Text = Resource1.Add;
+        }
+    }
+}
