27152c4cc7cefdadabcaf4bd8532d164fe7f1fd1
Takács Bálint <tbalint99@gmail.com>
2021-10-29 16:56:00 +0200

    GetRates() added

--- 6.gyak_wyaprm/6.gyak_wyaprm/6.gyak_wyaprm/Form1.cs
+++ 6.gyak_wyaprm/6.gyak_wyaprm/6.gyak_wyaprm/Form1.cs
@@ -1,7 +1,9 @@
-﻿using System;
+﻿using _6.gyak_wyaprm.MnbServiceReference;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -15,6 +17,24 @@ namespace _6.gyak_wyaprm
         public Form1()
         {
             InitializeComponent();
+            GetRates();
+        }
+
+        private void GetRates () {
+            var mnbService = new MNBArfolyamServiceSoapClient();
+
+            var request = new GetExchangeRatesRequestBody()
+            {
+                currencyNames = "EUR",
+                startDate = "2020-01-01",
+                endDate = "2020-06-30"
+            };
+
+            var response = mnbService.GetExchangeRates(request);
+
+            var result = response.GetExchangeRatesResult;
+
+            //Console.WriteLine(result);
         }
     }
 }
