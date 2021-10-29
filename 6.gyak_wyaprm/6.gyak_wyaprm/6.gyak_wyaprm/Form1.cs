036d96ef1c9355bebf4da4d6582b1a6e34cebeb9
Takács Bálint <tbalint99@gmail.com>
2021-10-29 17:07:42 +0200

    Xml loaded

--- 6.gyak_wyaprm/6.gyak_wyaprm/6.gyak_wyaprm/Form1.cs
+++ 6.gyak_wyaprm/6.gyak_wyaprm/6.gyak_wyaprm/Form1.cs
@@ -1,4 +1,5 @@
-﻿using _6.gyak_wyaprm.MnbServiceReference;
+﻿using _6.gyak_wyaprm.Entities;
+using _6.gyak_wyaprm.MnbServiceReference;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -9,18 +10,23 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml;
 
 namespace _6.gyak_wyaprm
 {
     public partial class Form1 : Form
     {
+        BindingList<RateData> Rates = new BindingList<RateData>();
+
         public Form1()
         {
             InitializeComponent();
             GetRates();
+            dataGridView1.DataSource = Rates;
         }
 
-        private void GetRates () {
+        private void GetRates ()
+        {
             var mnbService = new MNBArfolyamServiceSoapClient();
 
             var request = new GetExchangeRatesRequestBody()
@@ -35,6 +41,28 @@ namespace _6.gyak_wyaprm
             var result = response.GetExchangeRatesResult;
 
             //Console.WriteLine(result);
+
+            var xml = new XmlDocument();
+            xml.LoadXml(result);
+
+            foreach (XmlElement element in xml.DocumentElement)
+            {
+                var rate = new RateData();
+                Rates.Add(rate);
+
+                // Dátum
+                rate.Date = DateTime.Parse(element.GetAttribute("date"));
+
+                // Valuta
+                var childElement = (XmlElement)element.ChildNodes[0];
+                rate.Currency = childElement.GetAttribute("curr");
+
+                // Érték
+                var unit = decimal.Parse(childElement.GetAttribute("unit"));
+                var value = decimal.Parse(childElement.InnerText);
+                if (unit != 0)
+                    rate.Value = value / unit;
+            }
         }
     }
 }
