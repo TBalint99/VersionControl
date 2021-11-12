c992024ce08631911f887ff95813d04427128ad5
Takács Bálint <tbalint99@gmail.com>
2021-11-12 23:53:43 +0100

    classes added

---
+++ 7.gyak_wyaprm/7.gyak_wyaprm/Entities/CarFactory.cs
@@ -0,0 +1,18 @@
+﻿using _8het.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _8het.Entities
+{
+    public class CarFactory : ToyFactory
+    {
+        public Toy CreateNew()
+        {
+            Car car = new Car();
+            return car;
+        }
+    }
+}
